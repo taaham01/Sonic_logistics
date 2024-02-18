e26c664a7a0af7505eda45d0cc8b4df5111df037
FCC <FCC@DESKTOP-FV0R7R3>
2024-02-19 03:39:56 +0500

    Add project files.

---
+++ Sonic_logistics/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sonic_logistics.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
